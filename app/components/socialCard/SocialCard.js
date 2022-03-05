56f4f66a6b8b1904e27fce5a544c15f888a5f553
Vonkuro <vonkuro@gmail.com>
2022-03-05 23:19:28 +0100

    [WIP] trying to load image dynamicaly

--- app/components/socialCard/SocialCard.js
+++ app/components/socialCard/SocialCard.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import { StyleSheet, ImageBackground, View, Dimensions, Text, Button } from 'react-native';
+import { StyleSheet, ImageBackground, Image, View, Dimensions, Text, Button } from 'react-native';
+import * as config from "./socialCard.json";
 
 export default function SocialCard() {
     function Upload() {
         console.log("Call to Upload function");
         return;
     }
+    const {default: defaultLogo } = React.lazy(() => import(config.Card.default.logo));
+    const defaultLogoUri = Image.resolveAssetSource(defaultLogo).uri;
 
     function Display() {
         console.log("Call to Display function");
         return;
     }
-
+    console.log(config.Card.default.logo);
     return (
         <View style={styles.card}>
             <ImageBackground source={require('./Card_test/paysage.png')} style={styles.background} resizeMode="center">
-            </ImageBackground>
-            <Text style={styles.title}> Hello to you i</Text>
+            <Image source={require("./Card_test/logoJS.png")}></Image>
+            <Text style={styles.title}> Hello to you !</Text>
             <Text style={styles.title}> Lorem ipsum dolor sit amet, consectetur adipiscing elit. In vehicula arcu sed velit aliquet elementum. Suspendisse commodo quis lectus vitae egestas. In vestibulum interdum libero id egestas.</Text>
-            <Text style={styles.title}> http://link/to/site.com</Text>
-
+            </ImageBackground>
+            
 
             <Button title='Load' onPress={() => { Display() }} />
             <Button title='Save' onPress={() => { Upload() }} />
@@ -40,7 +43,7 @@ const styles = StyleSheet.create({
     },
     title:{
         fontSize:20,
-        color: '#FFFFFF',
+        color: '#FFFFFF'
     }
 });
 
